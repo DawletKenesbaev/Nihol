91c60e52f35896d103ad51fcd3a31d08421b8c0b
NUKUS777 <dawletkenesbaev620@gmail.com>
2024-01-17 19:08:03 +0500

    redux

--- src/tools/Wrapper/index.jsx
+++ src/tools/Wrapper/index.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';  // Import createStore from 'redux'
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import { AuthProvider } from 'react-auth-kit';
 import ru_RU from 'antd/locale/ru_RU';
 
 // Assuming you have a rootReducer for your Redux store
-import rootReducer from '../../redux';
-
 // Create the Redux store
-const store = createStore(rootReducer);
 
 function Wrapper({ children }) {
   return (
-    <Provider store={store}>
+    <Provider >
       <BrowserRouter>
-        <AuthProvider authName="_auth" authType="cookie" cookieDomain={window.location.hostname} cookieSecure={window.location.protocol === 'https:'} store={store}>
+        {/* <AuthProvider authName="_auth" authType="cookie" cookieDomain={window.location.hostname} cookieSecure={window.location.protocol === 'https:'} > */}
           <ConfigProvider locale={ru_RU}>
             {children}
           </ConfigProvider>
-        </AuthProvider>
+        {/* </AuthProvider> */}
       </BrowserRouter>
     </Provider>
   );
