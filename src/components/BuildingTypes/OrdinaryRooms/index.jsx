980626ae4820bb70f161cc654176201d62dbbb5b
NUKUS777 <dawletkenesbaev620@gmail.com>
2024-01-21 17:58:23 +0500

    working on buildings

--- src/components/BuildingTypes/OrdinaryRooms/index.jsx
+++ src/components/BuildingTypes/OrdinaryRooms/index.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import building from '../../../assets/images/building.svg'
+import { Outlet, useNavigate, useOutlet } from 'react-router-dom'
+import Card from '../../../generic/Card'
+import styled from 'styled-components'
 const OrdinaryRooms = () => {
-  return (
-    <div>
-      <img  src={building}/>
-    </div>
-  )
+    const navigate = useNavigate()
+    const HasOutlet = useOutlet()
+    return (
+      !HasOutlet ?
+       <>
+            <Wrapper>
+              <Card title={'Building 2'}  url={building}  onClick={()=> navigate('/empty-places/ordinary-rooms')}/>
+              <Card title={'Building 4'}  url={building}  onClick={()=> navigate('/empty-places/luxury-rooms')}/>
+              <Card title={'Building 6'}  url={building}  onClick={()=> navigate('/empty-places/cottages')}/>
+             </Wrapper>
+       </>:  <Outlet />
+  
+    ) 
 }
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr ;
+  width: 700px;
+  grid-row-gap:50px;
+  grid-column-gap:80px;
+  margin:0 auto;
+  align-items: center;
+  justify-content: center;
+`
+
 
 export default OrdinaryRooms
